dd42e14ea122af4b36a70b25c0eeff7a5a0696b5
Mehmet Sahin <mehmetshin96@gmail.com>
2022-05-14 13:58:42 -0400

    update transition and background color

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,30 @@
+import React, {useState, useEffect} from 'react'
 import "./Header.css";
 
 function Header() {
+    const [isTranspanrent, setIsTranspanrent] = useState(true);
+
+    const listenScrollEvent = () => {
+       if (window.scrollY > 50) {
+        setIsTranspanrent(false)
+       } else {
+        setIsTranspanrent(true)
+       }
+      }
+      
+      useEffect(() => {
+        window.addEventListener('scroll', listenScrollEvent);
+      
+        return () =>
+          window.removeEventListener('scroll', listenScrollEvent);
+      }, []);
+
     return (
-        <nav className="navbar navbar-expand-lg fixed-top p-3 bg-transparent">
+        <nav className={`navbar navbar-expand-lg p-3 fixed-top ${isTranspanrent ? "bg-transparent" : "navbar-shrink"}`}>
             <div className="container">
-                <a className="navbar-brand" href="#home" >Mehmet Sahin</a>
+                <a className="navbar-brand" style={{ fontSize: isTranspanrent ? "" : "1.5rem" }} href="#home" >
+                    <b> Mehmet Sahin </b>
+                </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
